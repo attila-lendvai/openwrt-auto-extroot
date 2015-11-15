61c95f104b8eb225d59bf5d4da5140079163ed12
Attila Lendvai <attila@lendvai.name>
2015-11-15 16:42:06 +0100

    separated the crucial and optional package in build.sh

--- build.sh
+++ build.sh
@@ -28,15 +28,24 @@ IMGTEMPDIR="${BUILD}/openwrt-build-image-extras"
 IMGBUILDERDIR="${BUILD}/OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64"
 IMGBUILDERURL="https://downloads.openwrt.org/chaos_calmer/15.05/ar71xx/generic/OpenWrt-ImageBuilder-15.05-ar71xx-generic.Linux-x86_64.tar.bz2"
 
-PREINSTALLED_PACKAGES="wireless-tools firewall iptables"
+# the minimally needed packages for the proper functioning of the auto extroot machinery.
+# kmod-fs-ext4  225k
+# e2fsprogs     182k
+# fdisk         100k
+# the rest are around 20-30k
+PREINSTALLED_PACKAGES="blkid block-mount kmod-usb2 kmod-usb-storage mount-utils swap-utils e2fsprogs kmod-fs-ext4 fdisk"
+
+# the following packages are optional, feel free to (un)comment them
+PREINSTALLED_PACKAGES+=" wireless-tools firewall iptables"
+PREINSTALLED_PACKAGES+=" kmod-usb-storage-extras kmod-mmc"
 PREINSTALLED_PACKAGES+=" ppp ppp-mod-pppoe ppp-mod-pppol2tp ppp-mod-pptp kmod-ppp kmod-pppoe"
-PREINSTALLED_PACKAGES+=" fdisk blkid swap-utils mount-utils block-mount e2fsprogs kmod-fs-ext4 kmod-usb2 kmod-usb-uhci kmod-usb-ohci kmod-usb-storage kmod-usb-storage-extras kmod-mmc"
+PREINSTALLED_PACKAGES+=" kmod-usb-uhci kmod-usb-ohci"
 PREINSTALLED_PACKAGES+=" luci"
 
 mkdir --parents ${BUILD}
 
 rm -rf $IMGTEMPDIR
-cp -r image-extras/common $IMGTEMPDIR
+cp -r image-extras/common/ $IMGTEMPDIR
 PER_PLATFORM_IMAGE_EXTRAS=image-extras/${TARGET_PLATFORM}/
 if [ -e $PER_PLATFORM_IMAGE_EXTRAS ]; then
     rsync -pr $PER_PLATFORM_IMAGE_EXTRAS $IMGTEMPDIR/
