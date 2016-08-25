2a043ca3176b15ef6ec9768fe865e904179cd12d
Sindhu S <sindhus@live.in>
2016-08-25 14:37:01 +0530

    Fix directory creation flags
    --parents doesn't work and stalls the program. 
    -pv is more correct.

--- build.sh
+++ build.sh
@@ -67,7 +67,7 @@ PREINSTALLED_PACKAGES+=" kmod-usb-storage-extras kmod-mmc"
 PREINSTALLED_PACKAGES+=" ppp ppp-mod-pppoe ppp-mod-pppol2tp ppp-mod-pptp kmod-ppp kmod-pppoe"
 PREINSTALLED_PACKAGES+=" luci"
 
-mkdir --parents ${BUILD}
+mkdir -pv ${BUILD}
 
 rm -rf $IMGTEMPDIR
 cp -r image-extras/common/ $IMGTEMPDIR
