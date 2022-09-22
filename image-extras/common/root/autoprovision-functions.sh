9060718dda79a3f1e77a549bb8090fb78e2e6066
Lance <Gero3977@gmail.com>
2022-09-22 03:37:53 -0700

    Add WD My Net n750 LEDs
    This solves https://github.com/attila-lendvai/openwrt-auto-extroot/issues/22

--- image-extras/common/root/autoprovision-functions.sh
+++ image-extras/common/root/autoprovision-functions.sh
@@ -36,6 +36,10 @@ case $(board_name) in
         autoprovisionUSBLed="green:wlan2g"
         autoprovisionStatusLed="green:wlan5g"
         ;;
+    *mynet-n750*)
+        autoprovisionUSBLed="blue:wps"
+        autoprovisionStatusLed="blue:wireless"
+        ;;
 esac
 
 
