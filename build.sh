1744fe95318d88eb6332527f509e8bd91e12b902
Attila Lendvai <attila@lendvai.name>
2023-11-16 11:05:20 +0100

    use OpenWrt 23.05.2
    untested

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ BUILD="$(readlink -f "${BUILD}")"
 ###
 ### chose a release
 ###
-RELEASE="23.05.0"
+RELEASE="23.05.2"
 
 IMGBUILDER_NAME="openwrt-imagebuilder-${RELEASE}-${TARGET_ARCHITECTURE}-${TARGET_VARIANT}.Linux-x86_64"
 IMGBUILDER_DIR="${BUILD}/${IMGBUILDER_NAME}"
